880d3145e95f5cd35564085d13ec6048ad13f63f
Hay Kranen <huskyr@gmail.com>
2020-04-28 23:01:39 +0200

    saving this before doing language refactor

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 (function() {
     function $(selector) {
+        return document.querySelector(selector);
+    }
+
+    function $$(selector) {
         var els = document.querySelectorAll(selector);
         return Array.prototype.slice.call(els);
     }
 
+    function addTouchClass() {
+        if ('ontouchend' in window) {
+            $("html").classList.add('has-touch');
+        } else {
+            $("html").classList.add('has-mouse');
+        }
+    }
+
     function initToggle() {
-        $("[data-toggle]").forEach(function(el) {
+        $$("[data-toggle]").forEach(function(el) {
             el.addEventListener('click', function(e) {
                 e.preventDefault();
-                var el = $(e.target.attributes.href.value)[0]
+                var el = $(e.currentTarget.attributes.href.value);
                 var toggleState = el.dataset.toggleState;
                 el.dataset.toggleState = toggleState === 'hidden' ? 'show' : 'hidden';
             }, false);
@@ -36,9 +48,10 @@
     }
 
     function main() {
-        $("html")[0].className = "js-enabled";
-        $("#search")[0].focus();
+        $("html").classList.add('has-js');
+        $("#search").focus();
         initToggle();
+        addTouchClass();
         konami();
     }
 
